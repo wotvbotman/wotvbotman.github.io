fb083af4ba053e01330ff5a7529d6181a197b025
wotvbtman <wotvbotman@protonmail.com>
2021-04-28 11:10:20 -0500

    Production environment

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/cache-list.js');
 
-var cacheName = 'chirpy-20210428.0644';
+var cacheName = 'chirpy-20210428.1109';
 
 function isExcluded(url) {
   const regex = /(^http(s)?|^\/)/; /* the regex for CORS url or relative url */
