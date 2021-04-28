ac95b620ef74913d7e32025232e67d3927bcef09
wotvbtman <wotvbotman@protonmail.com>
2021-04-28 00:45:51 -0500

    Updated description.

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/cache-list.js');
 
-var cacheName = 'chirpy-20210427.2338';
+var cacheName = 'chirpy-20210428.0045';
 
 function isExcluded(url) {
   const regex = /(^http(s)?|^\/)/; /* the regex for CORS url or relative url */
