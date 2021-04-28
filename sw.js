4c72f3dbc8a628335896edc879a48cfbd9774023
wotvbtman <wotvbotman@protonmail.com>
2021-04-28 01:05:03 -0500

    Small edits for SEO

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/cache-list.js');
 
-var cacheName = 'chirpy-20210428.0045';
+var cacheName = 'chirpy-20210428.0104';
 
 function isExcluded(url) {
   const regex = /(^http(s)?|^\/)/; /* the regex for CORS url or relative url */
